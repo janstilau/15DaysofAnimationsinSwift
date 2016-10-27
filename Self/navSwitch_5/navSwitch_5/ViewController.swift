5a01273c12826d37f5450a3539b6e7d7f3873f28
janstilau <janstilau@gmail.com>
2016-10-27 18:27:38 +0800

    begin_6

---
+++ Self/navSwitch_5/navSwitch_5/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  navSwitch_5
+//
+//  Created by jansti on 16/10/27.
+//  Copyright © 2016年 jansti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+  
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
---
+++ Self/navSwitch_5/navSwitch_5Tests/navSwitch_5Tests.swift
@@ -0,0 +1,36 @@
+//
+//  navSwitch_5Tests.swift
+//  navSwitch_5Tests
+//
+//  Created by jansti on 16/10/27.
+//  Copyright © 2016年 jansti. All rights reserved.
+//
+
+import XCTest
+@testable import navSwitch_5
+
+class navSwitch_5Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
